d1fb06bbe47a819cabc47741d34ef3518782d689
Randy Roger <rfamos53@gmail.com>
2022-05-03 15:20:31 -0700

    adding index files.

--- static/js/app.js
+++ static/js/app.js
@@ -22,3 +22,32 @@ function buildTable(data) {
   });
 }
 
+// Add a function to use D3 (Data-Driven Documents) to handle an action from a user, such as a button click
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    // set default filter and create a new variable
+    let filteredData = tableData;
+
+    // add IF statement to check for a date filter, otherwise use default
+    // A triple equal sign (===) is checking for strict equality. 
+    // This means that the type and value have to match perfectly.
+    // A double equals sign (==) is checking for loose equality
+    if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+      }
+    
+       // Rebuild the table using the filtered data
+      // @NOTE: If no date was entered, then filteredData will
+      // just be the original tableData.
+      buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
+
